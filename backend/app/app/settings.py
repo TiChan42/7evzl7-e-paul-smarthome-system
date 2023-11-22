5166fad1f59adea8809c104fb720d2ba4f0a2055
Link640 <robbe0503@t-online.de>
2023-11-22 08:15:23 +0000

    - added django rest Framework

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'e_paul_smarthome_system',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
