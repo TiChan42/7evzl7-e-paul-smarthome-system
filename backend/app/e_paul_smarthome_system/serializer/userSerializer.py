68045d73926c0ee8ce34e03845a1d833ef212b61
David Fischer <davidfischer139@gmail.com>
2024-01-12 12:43:37 +0100

    Testing for improving the detail of responses

--- backend/app/e_paul_smarthome_system/serializer/userSerializer.py
+++ backend/app/e_paul_smarthome_system/serializer/userSerializer.py
@@ -1,8 +1,14 @@
 from ..model.user import User
 from rest_framework import serializers
+from .microcontrollerSerializer import MicrocontrollerSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id','firstname','lastname','email','password','key','gender','birthdate']
   
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','firstname','lastname','email','password','key','gender','birthdate','microcontroller']
+    microcontroller = MicrocontrollerSerializer(many=True)
