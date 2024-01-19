093848a35cb73a4a485f04b5de0372cea05e9335
Link640 <robbe0503@t-online.de>
2024-01-19 17:42:19 +0000

    added account counter to about us page

--- backend/app/e_paul_smarthome_system/urls.py
+++ backend/app/e_paul_smarthome_system/urls.py
@@ -2,10 +2,12 @@ from django import urls
 
 from .views.userView import UserView, SingleUserView
 from .views.accountView import AccountView
+from .views.about_us import AboutUsView
 from django.urls import path
 
 urlpatterns = [
     path("user", UserView.as_view(), name = "user"),
     path("user/<userid>", SingleUserView.as_view(), name = "singleUser"),
     path("account", AccountView.as_view(), name = "group"),
+    path("about_us", AboutUsView.as_view(), name="aboutUs")
 ]
