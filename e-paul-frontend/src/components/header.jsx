1b733bb7ed5ddb5dfe1b59c78c5b6004c28488d2
LinusFred18 <linus.fred.314@gmail.com>
2024-01-21 19:26:39 +0100

    created header.css

--- e-paul-frontend/src/components/header.jsx
+++ e-paul-frontend/src/components/header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './header.css'
 
 class Header extends Component {
     state = {  } 
@@ -7,13 +8,12 @@ class Header extends Component {
             <nav className="navbar" style={{backgroundColor: '#00BFFF'}}>
             <div className="container-fluid">
                 <a className="navbar-brand" href="www.google.com">
-                    <img src="assets/img/logo.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top"/>
-                    E-Paul 7EVZL7
+                    <img src="assets/img/clearLogo.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top"/>
+                    <span className="me-2"> E-Paul 7EVZL7</span>
                 </a>
-                <a className="nav-link active ms-auto" aria-current="page" href="www.google.com">About us</a>
                 <form className="ms-auto" style={{color: '#00BFFF'}}>
-                    <button className="btn btn-outline-success me-2" type="button">Sign in</button>
-                    <button className="btn btn-outline-success me-2" type="button">Login</button>
+                    <button className="btn btn-outline-success me-2 btn-sign-in" type="button">Sign in</button>
+                    <button className="btn btn-outline-success me-2 btn-login" type="button">Login</button>
                 </form>
             </div>
             </nav>    
