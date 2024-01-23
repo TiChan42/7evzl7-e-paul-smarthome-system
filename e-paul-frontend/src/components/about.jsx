465bba42c40c1fad433bdc6fb7d98726580a8278
LinusFred18 <linus.fred.314@gmail.com>
2024-01-23 16:21:34 +0100

    Test Email Support System, background

--- e-paul-frontend/src/components/about.jsx
+++ e-paul-frontend/src/components/about.jsx
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import './about.css'
 
 class About extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <h1>Example About Us Page</h1>
-        );
-    }
+  state = {
+    isPopupOpen: false,
+    message: '',
+  };
+
+  openPopup = () => {
+    this.setState({ isPopupOpen: true, message: 'Bitte schreiben Sie uns Ihr Anliegen:' });
+  };
+
+  closePopup = () => {
+    this.setState({ isPopupOpen: false });
+  };
+
+  sendEmail = () => {
+    const emailBody = encodeURIComponent(this.state.message);
+    const emailSubject = encodeURIComponent('E-Paul Kundensupport');
+    const emailAddress = 'mddruica@gmail.com';
+
+    // Simuliere das Öffnen des E-Mail-Clients mit vordefiniertem Betreff und Nachricht
+    window.open(`mailto:${emailAddress}?subject=${emailSubject}&body=${emailBody}`);
+
+    // Schließe das Popup nach dem Senden der E-Mail
+    this.closePopup();
+  };
+
+  render() {
+    return (
+      <div className='background-container'>
+        <h1 className='headline'>Example About Us Page</h1>
+        <button className="btn btn-outline-success me-2 btn-message" onClick={this.openPopup}>Nachricht schreiben</button>
+
+        {this.state.isPopupOpen && (
+          <div className="popup">
+            <p>{this.state.message}</p>
+            <button onClick={this.sendEmail} className="btn btn-outline-success me-2 btn-email">Zur E-Mail</button>
+            <button onClick={this.closePopup} >Schließen</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
- 
+
 export default About;
