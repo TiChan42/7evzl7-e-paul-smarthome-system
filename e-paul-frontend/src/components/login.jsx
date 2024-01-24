05877d8706030ae4e6ac271e9d0c3db9ff5c0932
77hrs <e.medwedkin@web.de>
2024-01-24 16:17:41 +0100

    login und signin hinzugefügt

--- e-paul-frontend/src/components/login.jsx
+++ e-paul-frontend/src/components/login.jsx
@@ -4,7 +4,28 @@ class Login extends Component {
     state = {  } 
     render() { 
         return (
-            <h1>Login</h1>
+            <div className="container">
+                <form>
+                    <h4 className="text-center">Anmelden</h4>
+                    <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email Adresse</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+
+                    <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Passwort</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <p><a className="link-opacity-75-hover" href="#">Passwort vergessen</a></p>
+                    <p><a className="link-opacity-75-hover" href="SignUp.jsx">Jetzt registrieren!</a></p>
+                    </div>
+
+                    <div className="mb-3">
+                    <button type="submit" className="btn btn-primary">Anmelden</button>
+                    </div>
+                    
+                </form>
+            </div>
+            
         );
     }
 }
--- e-paul-frontend/src/components/signIn.jsx
+++ e-paul-frontend/src/components/signIn.jsx
@@ -4,7 +4,28 @@ class SignIn extends Component {
     state = {  } 
     render() { 
         return (
-            <h1>Sign In</h1>
+            <div className="container">
+                <form>
+                    <h4 className="text-center">Registrierung</h4>
+                    <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email Adresse</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+
+                    <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Passwort</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <label htmlFor="exampleInputPassword1" className="form-label">Passwort wiederholen</label>
+                    <input type="password" className="form-control" id="exampleInputPassword2" />
+                    <p><a className="link-opacity-75-hover" href="#">Bereits registriert?</a></p>
+                    </div>
+
+                    <div className="mb-3">
+                    <button type="submit" className="btn btn-primary">Anmelden</button>
+                    </div>
+                    
+                </form>
+            </div>
         );
     }
 }
