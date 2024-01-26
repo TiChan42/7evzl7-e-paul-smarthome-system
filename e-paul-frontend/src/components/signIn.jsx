0017e00b0ba50393454da2eaa587a488052a91cb
77hrs <e.medwedkin@web.de>
2024-01-26 10:57:29 +0100

    Update signIn.jsx

--- e-paul-frontend/src/components/signIn.jsx
+++ e-paul-frontend/src/components/signIn.jsx
@@ -5,16 +5,16 @@ class SignIn extends Component {
     render() { 
         return (
             <div className="container">
-                <form>
+                <form action="/action_page.php" method="post" target="_blank">
                     <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email Adresse</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email eingeben" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Email eingeben" />
                     </div>
 
                     <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Passwort</label>
+                    <label htmlFor="exampleInputPassword1" className="form-label" name="password">Passwort</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Passwort eingeben" />
-                    <label htmlFor="exampleInputPassword1" className="form-label">Passwort wiederholen</label>
+                    <label htmlFor="exampleInputPassword1" className="form-label" name="passwordRepeat">Passwort wiederholen</label>
                     <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Passwort wiederholen" />
                     <p><a className="link-opacity-75-hover" href="/login">Bereits registriert?</a></p>
                     </div>
