de37d0963e170b63b5a20e5530329e2906144997
77hrs <e.medwedkin@web.de>
2024-01-30 14:48:37 +0100

    POST SignUp
    Wenn man registrieren drückt, werden die Daten an die Serveradresse(fehlt noch) in ein JSON Format umgewandelt und gesendet

--- e-paul-frontend/src/components/signIn.jsx
+++ e-paul-frontend/src/components/signIn.jsx
@@ -1,32 +1,52 @@
 import React, { Component } from 'react';
 
 class SignIn extends Component {
-    state = {  } 
+    componentDidMount() {
+        const formEl = document.querySelector(".form");
+        formEl.addEventListener("submit", this.handleSubmit);
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+
+        fetch("http://localhost:3000/signin", {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+          .then(data => console.log(data))
+          .catch(error => console.log(error))
+    }
+
     render() { 
         return (
             <div className="container">
-                <form action="/action_page.php" method="post" target="_blank">
+                <form className="form" onSubmit={this.handleSubmit}>
                     <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email Adresse</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Email eingeben" />
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email Adresse</label>
+                        <input type="email" className="form-control" id="email1" name="email" aria-describedby="emailHelp" placeholder="Email eingeben" />
                     </div>
 
                     <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label" name="password">Passwort</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Passwort eingeben" />
-                    <label htmlFor="exampleInputPassword1" className="form-label" name="passwordRepeat">Passwort wiederholen</label>
-                    <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Passwort wiederholen" />
-                    <p><a className="link-opacity-75-hover" href="/login">Bereits registriert?</a></p>
+                        <label htmlFor="exampleInputPassword1" className="form-label">Passwort</label>
+                        <input type="password" className="form-control" name="password" id="pw1" placeholder="Passwort eingeben" />
+                        <label htmlFor="exampleInputPassword1" className="form-label">Passwort wiederholen</label>
+                        <input type="password" className="form-control" name="passwordRepeat" id="pw2" placeholder="Passwort wiederholen" />
+                        <p><a className="link-opacity-75-hover" href="/login">Bereits registriert?</a></p>
                     </div>
 
                     <div className="mb-3">
-                    <button type="submit" className="btn btn-primary">Registrieren</button>
+                        <button type="submit" className="btn btn-primary">Registrieren</button>
                     </div>
-                    
                 </form>
             </div>
         );
     }
 }
- 
+
 export default SignIn;
