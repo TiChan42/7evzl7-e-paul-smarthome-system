52cfab6952cc8774ecb2a1a977d5f4f3b3266647
Link640 <robbe0503@t-online.de>
2024-01-31 14:42:27 +0000

    removed frontend after testing

--- e-paul-frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDom from "react-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
-import App from "./App";
-
-
-ReactDom.render(<App/>, document.getElementById('root'));
