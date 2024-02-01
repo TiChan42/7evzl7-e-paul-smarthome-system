e24470289e4d16afcb21f129f8c4b933429878cb
Jonas <64211998+Sneggel@users.noreply.github.com>
2024-02-01 09:35:02 +0100

    H

--- e-paul-frontend/src/App.jsx
+++ e-paul-frontend/src/App.jsx
@@ -6,22 +6,24 @@ import Footer from "./components/footer";
 import Login from "./components/login";
 import SignIn from "./components/signIn";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 class App extends Component {
     state = {  } 
     render() { 
         return (
-            <Router>
-                <Header/>
-                <Routes>
-                    <Route exact path="/" element={<Middle/>}/>
-                    <Route path="/about" element={<About/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/signin" element={<SignIn/>}/>
-                </Routes>
-                <Footer/>
-            </Router>
-
+            <ChakraProvider>
+                <Router>
+                    <Header/>
+                    <Routes>
+                        <Route exact path="/" element={<Middle/>}/>
+                        <Route path="/about" element={<About/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/signin" element={<SignIn/>}/>
+                    </Routes>
+                    <Footer/>
+                </Router>
+            </ChakraProvider>
         );
     }
 }
