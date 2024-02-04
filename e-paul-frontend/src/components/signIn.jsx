bfe1122e3d4f1d3517a1a5263a5cdea683c8b565
77hrs <e.medwedkin@web.de>
2024-02-04 13:41:13 +0100

    Sign Up mit Chakra UI verknüpft

--- e-paul-frontend/src/components/signIn.jsx
+++ e-paul-frontend/src/components/signIn.jsx
@@ -4,29 +4,58 @@ import { Link } from 'react-router-dom';
 
 
 class SignIn extends Component {
-    render() { 
+
+    //check if form is in DOM
+    componentDidUpdate() {
+        const formEl = document.querySelector(".form");
+        if (formEl) {
+            formEl.addEventListener("submit", this.handleSubmit);
+        }
+    }
+
+    //POST the content of form as JSON
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+
+        fetch("http://localhost:3000/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }
+
+    render() {
         return (
-            <Center>
-                <Card size="lg" width={400}>
-                    <CardBody>
-                        <Text>Email</Text>   
-                        <Input placeholder='Email eingeben' size='md' variant={"filled"}/>
-                        <br/>
-                        <br/>
-                        <Text>Passwort</Text>   
-                        <Input placeholder='Passwort eingeben' size='md' variant={"filled"}/>
-                        <br/>
-                        <br/>
-                        <Text>Passwort wiederholen</Text>   
-                        <Input placeholder='Passwort wiederholen' size='md' variant={"filled"}/>
-                        <Link to="/login"> Bereits registriert? </Link>
-                        <br/>
-                    </CardBody>
-                    <Center p={3}>
-                        <Button colorScheme='teal'>Anmelden</Button>
-                    </Center>
-                </Card>
-            </Center>
+            <form className="form" onSubmit={this.handleSubmit}>
+                <Center>
+                    <Card size="lg" width={400}>
+                        <CardBody>
+                            <Text>Email</Text>
+                            <Input type="email" name="email" placeholder='Email eingeben' size='md' variant={"filled"} />
+                            <br />
+                            <br />
+                            <Text>Passwort</Text>
+                            <Input type="password" name="password" placeholder='Passwort eingeben' size='md' variant={"filled"} />
+                            <br />
+                            <br />
+                            <Text>Passwort wiederholen</Text>
+                            <Input type="password" name="passwordRepeat" placeholder='Passwort wiederholen' size='md' variant={"filled"} />
+                            <Link to="/login"> Bereits registriert? </Link>
+                            <br />
+                        </CardBody>
+                        <Center p={3}>
+                            <Button type="submit" colorScheme='teal'>Anmelden</Button>
+                        </Center>
+                    </Card>
+                </Center>
+            </form>
         );
     }
 }
