1ec6cecc693612736beee011ff75588ec5000008
David Fischer <davidfischer139@gmail.com>
2024-02-08 22:06:53 +0100

    started working on logSerializer

--- backend/app/e_paul_smarthome_system/serializer/userSerializer.py
+++ backend/app/e_paul_smarthome_system/serializer/userSerializer.py
@@ -1,6 +1,7 @@
 from ..model.user import User
 from rest_framework import serializers
 from .microcontrollerSerializer import MicrocontrollerSerializer
+from .logSerializer import LogSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,4 +23,7 @@ class UserHomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'pictureid']
+        #fields = ['username', 'pictureid', 'log']
+    #log = LogSerializer(many=True)
+    # In doku nachschauen wie das genau geht
 
