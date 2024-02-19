db23ff2bd17350318f70c420d28bf70fdb964fa6
Link640 <robbe0503@t-online.de>
2024-02-19 08:06:24 +0000

    Revert "removed frontend after testing"
    This reverts commit 52cfab6952cc8774ecb2a1a977d5f4f3b3266647.

---
+++ e-paul-frontend/src/components/login.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css'
+
+class Login extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="container">
+                <form>
+                    <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email Adresse</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email eingeben" />
+                    </div>
+
+                    <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Passwort</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Passwort eingeben" />
+                    <p><a className="link-opacity-75-hover" href="">Passwort vergessen</a></p>
+                    <Link to="/signin"> Jetzt registieren! </Link>
+                    </div>
+
+                    <div className="mb-3">
+                    <button type="submit" className="btn btn-primary">Anmelden</button>
+                    </div>
+                    
+                </form>
+            </div>
+            
+        );
+    }
+}
+ 
+export default Login;
