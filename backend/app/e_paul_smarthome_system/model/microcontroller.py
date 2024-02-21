793a5049989c7ddf1b16183f5609c031caa8f6a0
Link640 <robbe0503@t-online.de>
2024-02-21 15:33:53 +0000

    - added Device View - microcontroller is now in a Relation with Account and not user
    - changed Serializers to work with new changes

--- backend/app/e_paul_smarthome_system/model/microcontroller.py
+++ backend/app/e_paul_smarthome_system/model/microcontroller.py
@@ -3,5 +3,5 @@ from django.db import models
 class Microcontroller(models.Model):
     key = models.CharField(max_length=20, unique=True, null = True, blank = True)
     name = models.CharField(max_length=50)
-    User = models.ForeignKey("e_paul_smarthome_system.User", related_name="microcontroller" ,verbose_name=("User"), on_delete=models.CASCADE)
+    account = models.ForeignKey("e_paul_smarthome_system.Account", related_name="microcontroller" ,verbose_name=("User"), on_delete=models.CASCADE)
     #filter = models.ForeignKey()
---
+++ backend/app/e_paul_smarthome_system/views/devices.py
@@ -0,0 +1,21 @@
+from ..model.account import Account
+from ..serializer.accountSerializer import AccountMicrocontrollerSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class DeviceView(APIView):
+    queryset = Account.objects.all()
+
+    def post(self, request):
+        id = request.data['id']
+        queryset = Account.objects.get(pk = id)
+        serializer = AccountMicrocontrollerSerializer(queryset)
+        return Response(serializer.data, status = 200)
+
+"""
+teststring
+{
+    "id" : 1
+}
+"""
