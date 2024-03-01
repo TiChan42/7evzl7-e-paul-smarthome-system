f3c70e1e20e7eb3e1fc448fb98ea662611f20214
Jonas <lehmjonas03@gmail.com>
2024-03-01 09:57:58 +0100

    made footer links bold

--- e-paul-frontend/src/components/pages/footer.jsx
+++ e-paul-frontend/src/components/pages/footer.jsx
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import { Flex, Link } from '@chakra-ui/react';
 
 class Footer extends Component {
-    state = {  } 
     render() { 
-        return (
-            
-                <Flex bg={"#218395"} position={'fixed'} w={"100%"} bottom={"0px"} justifyContent="space-between" minHeight={'50px'} borderTop={"1px"} borderTopColor={"#f8fafc"}>
-                        <Link href="/imprint" p={"2"} color={'whitesmoke'} _hover={{}}>Impressum</Link>
-                        <Link href="/about" p={"2"} color={'whitesmoke'} _hover={{}}>About us</Link>
-                </Flex>
-            
+        return (  
+            <Flex bg={"#218395"} position={'fixed'} w={"100%"} bottom={"0px"} justifyContent="space-between" minHeight={'50px'} borderTop={"1px"} borderTopColor={"#f8fafc"}>
+                <Link href="/imprint" p={"3"} color={'whitesmoke'} _hover={{}} fontWeight={'bold'}>Impressum</Link>
+                <Link href="/about" p={"3"} color={'whitesmoke'} _hover={{}} fontWeight={'bold'}>About us</Link>
+            </Flex> 
         );
     }
 }
-
 export default Footer;
