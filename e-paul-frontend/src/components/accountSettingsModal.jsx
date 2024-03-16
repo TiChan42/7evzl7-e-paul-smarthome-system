20c291618266664ccd949eba6bf18267af17ddd5
David Fischer <davidfischer139@gmail.com>
2024-03-16 11:57:09 +0100

    added account deletion functionality

--- e-paul-frontend/src/components/accountSettingsModal.jsx
+++ e-paul-frontend/src/components/accountSettingsModal.jsx
@@ -12,7 +12,8 @@ import {
     Heading,
     Box,
     Text,
-    Input
+    Input,
+    useToast
 } from '@chakra-ui/react'
 import React from 'react';
 import { decryptString } from '../encryptionUtils';
@@ -26,6 +27,8 @@ const AccountSettingsModal = (props) => {
     const [openValidateDeleteAcc, setOpenValidateDeleteAcc] = React.useState(false)
     //Hier die ganzen Funktionen Hinpacken 
 
+    const executingUserID = decryptString(sessionStorage.getItem('executingUserID'));
+    const toast = useToast()
 
     const initialRef = React.useRef();
 
@@ -71,7 +74,34 @@ const AccountSettingsModal = (props) => {
         }
     };
 
-
+    const deleteAccount = async () => {
+        // API Request
+        const res = await fetch('http://epaul-smarthome.de:8000/api/deleteAccount', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                executingUserId: parseInt(executingUserID),
+                accountId: parseInt(accountID)
+            })
+        })
+        console.log(res)
+        // Clear Session Storage
+        sessionStorage.clear()
+        // Toast and Page reload
+        var waitTime = 2000
+        toast({
+            title: 'Account erfolgreich gelöscht',
+            description: 'Ihr Account wurde erfolgreich gelöscht. Sie werden in Kürze zur Startseite weitergeleitet.',
+            status: 'success',
+            duration: waitTime,
+            isClosable: true
+        });
+        // Reload Page
+        setTimeout(function () { window.location.reload() }, waitTime)
+    }
 
 
     return (
@@ -99,36 +129,34 @@ const AccountSettingsModal = (props) => {
                                     type="text"
                                     errorBorderColor='white'
                                     borderColor={'green'}
-                                    placeholder='Aktuelle E-Mail-Adresse'
-                                    _placeholder={{ color: 'white' }}
-                                    focusBorderColor={'red'}
-                                    pattern="/^\S+@\S+\.\S+$/"
-                                    marginTop={'2em'}
-                                    mailName = "oldMail"
-                                    state = {this}
-                                />
-
-                                <Input
-                                    isInvalid
-                                    type="text"
-                                    errorBorderColor='white'
-                                    borderColor={'green'}
                                     placeholder='Neue E-Mail-Adresse'
                                     _placeholder={{ color: 'white' }}
                                     focusBorderColor={'red'}
                                     pattern="/^\S+@\S+\.\S+$/"
                                     marginTop={'2em'}
-                                    mailName = "newMail"
-                                    state = {this}
+                                    mailName="newMail"
+                                    state={this}
 
                                 />
-
+                                <Input
+                                    isInvalid
+                                    type="text"
+                                    errorBorderColor='white'
+                                    borderColor={'green'}
+                                    placeholder='Ihr Pin'
+                                    _placeholder={{ color: 'white' }}
+                                    focusBorderColor={'red'}
+                                    pattern="/^\S+@\S+\.\S+$/"
+                                    marginTop={'2em'}
+                                    mailName="oldMail"
+                                    state={this}
+                                />
                                 <Button onClick={updateEmail} margin={'2em'} align={'left'} colorScheme='whiteAlpha' variant='solid' fontSize={[12, 12, 16]}>Bestätigen</Button>
                                 <br></br><br></br>
                                 <Text color={"white"}>Hier können Sie Ihren Account löschen:</Text>
                                 {/*<DeleteAcc />*/}
                                 <Button onClick={() => setOpenValidateDeleteAcc(true)} colorScheme='red' variant='solid' marginLeft={'2em'} marginTop={'1em'}>Account löschen</Button>
-                                <ValidateActionModal openModal = {openValidateDeleteAcc} closeModal = {()=>{setOpenValidateDeleteAcc(false)}} title = {"Account löschen?"} content = {"Möchten Sie wirklich Ihren E-Paul Account löschen? Diese Aktion kann nicht rückgängig gemacht werden."} execute = {()=>{console.log("passt")}}/>
+                                <ValidateActionModal openModal={openValidateDeleteAcc} closeModal={() => { setOpenValidateDeleteAcc(false) }} title={"Account löschen?"} content={"Möchten Sie wirklich Ihren E-Paul Account löschen? Diese Aktion kann nicht rückgängig gemacht werden."} execute={deleteAccount} />
                             </Box>
                         </Card>
                     </ModalBody>
