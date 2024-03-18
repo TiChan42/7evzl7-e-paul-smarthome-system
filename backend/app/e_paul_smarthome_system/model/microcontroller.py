c858915cb1b285f3649b4341e0e1fcbfe9ddc59d
Link640 <robbe0503@t-online.de>
2024-03-18 12:56:43 +0000

    - added new ERM - added missing Foreign Key in portTemplate

--- backend/app/e_paul_smarthome_system/model/microcontroller.py
+++ backend/app/e_paul_smarthome_system/model/microcontroller.py
@@ -5,4 +5,3 @@ class Microcontroller(models.Model):
     name = models.CharField(max_length=20, default = "ESP8266")
     type = models.CharField(max_length=32)
     account = models.ForeignKey("e_paul_smarthome_system.Account", related_name="microcontroller" ,verbose_name=("Account"), on_delete=models.CASCADE)
-    #filter = models.ForeignKey()
